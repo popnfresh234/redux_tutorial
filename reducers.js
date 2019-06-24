6e8f6f88745f9822997aec076268108e381adecb
Alex <ajhollid@gmail.com>
2019-06-24 10:16:34 -0700

    initial commit

---
+++ reducers.js
@@ -0,0 +1,47 @@
+import {
+  VisibilityFilters,
+  SET_VISIBILITY_FILTER,
+  ADD_TODO,
+  TOGGLE_TODO,
+} from './actions';
+
+const initialState = {
+  visibilityFilter: VisibilityFilters.SHOW_ALL,
+  todos: [],
+};
+
+function todoApp( state = iniitalState, action ) {
+  switch ( action.type ) {
+    case SET_VISIBILITY_FILTER:
+      return Object.assign( {}, state, {
+        visibilityFilter: action.filter,
+      } );
+
+    case ADD_TODO:
+      return Object.assign( {}, state, {
+        todos: [
+          ...state.todos,
+          {
+            text: action.text,
+            completed: false,
+          },
+        ],
+      } );
+
+    case TOGGLE_TODO:
+      return Object.assign( {}, state, {
+        todos: state.todos.map( ( todo, index ) => {
+          if ( index === action.index ) {
+            return Object.assign( {}, todo, {
+              completed: !todo.completed,
+            } );
+          }
+          return todo;
+        } ),
+      } );
+
+    default:
+      return state;
+  }
+}
+
